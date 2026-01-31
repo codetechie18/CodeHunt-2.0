55e8ceee9cacb4da5e00eda97f7007e4a6881c4e
Pranav <pranavtekade250107@gmail.com>
2026-02-01 01:04:53 +0530

    folder structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 '/vite.svg'
 import './App.css'
-import Hero from './components/Hero'
+import Hero from './home/Hero'
 import Section1 from './components/Section1'
-import Section2 from './components/Section2'
+import Section2 from './about/Section2'
 
 function App() {
 
